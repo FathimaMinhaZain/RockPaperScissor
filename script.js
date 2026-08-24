cce0a86562304534962b0825b8435a5749ef2422
FathimaMinhaZain <fathimaminhazain@gmail.com>
2026-08-24 13:40:45 +0300

    getComputerChoice

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('hellooo');
+function getComputerChoice(){
+    let n = Math.random();
+
+    if (n<0.3){
+        console.log('Rock');
+    } else if (n<0.6){
+        console.log('Paper');        
+    } else{
+        console.log('Scissor');        
+    }
+}
+
+getComputerChoice();
